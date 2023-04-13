bddd12612b924659f21499ee8406f377db3169ed
Kevin David <kevincarld@gmail.com>
2023-04-13 11:17:21 +1000

    env meta updated

--- next.config.js
+++ next.config.js
@@ -15,19 +15,18 @@ const nextConfig = {
   },
   env: {
     appMeta: {
-      canonical: 'canonical in next.config.js', // https://brandedcontent.[masthead].com.au/xxx/xxx"
-      brand: 'brand in next.config.js', // e.g Black Hawk
-      title: 'title in next.config.js', // e.g Being a good pet parent starts at mealtime
-      description: 'description in next.config.js',
-      siteName: 'siteName Title in next.config.js', // The Sydney Morning Herald, Good Food
-      icon: 'smh', // smh, goodfood, nine, afr, traveller
+      canonical: 'https://brandedcontent.nine.com.au/travel-guides/win-20k-holiday-travel-money-oz', // https://brandedcontent.[masthead].com.au/xxx/xxx"
+      brand: 'Travel Guides', // e.g Black Hawk
+      title: 'Win a $20,000 holiday!', // e.g Being a good pet parent starts at mealtime
+      description: 'You could be off on your dream holiday brought to you by Travel Money Oz - the leading foreign exchange service by travellers, for travellers.Wherever your travels take you, Travel Money Oz has the foreign currency you’ll need for your holiday.',
+      siteName: 'Nine', // The Sydney Morning Herald, Good Food
       themeColour: '#0a1633', // hex
-      ogImage: 'https://live-native-paid-content.pantheonsite.io/wp-content/uploads/2022/12/lecreuset-social-media-bg-1.jpg',
-      date: 'dd-mm-yy', // e.g 03-12-23
-      year: 'yyyy' // e.g 2023
+      ogImage: 'https://live-native-paid-content.pantheonsite.io/wp-content/uploads/2023/04/travel-money-social-0423.jpg',
+      date: '19-04-23', // e.g 03-12-23
+      year: '2023' // e.g 2023
     },
-    jobCode: '11111-name', // used for nine ga scripts
-    guidelines: false, // shows or hides layout guideline bars
+    jobCode: '11595-travel-money-comp', // used for nine ga scripts
+    guidelines: true, // shows or hides layout guideline bars
   }
 }
 
