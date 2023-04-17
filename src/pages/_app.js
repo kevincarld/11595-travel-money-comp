b0c208e6382cf0eb8bfe0dd71806b810fadd720f
Kevin David <kevincarld@gmail.com>
2023-04-17 11:57:30 +1000

    fix app meta and icons

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,6 +10,8 @@ export default function App({ Component, pageProps }) {
   const brand = process.env.appMeta?.brand
   const title = process.env.appMeta?.title
   const siteName = process.env.appMeta?.siteName
+  const canonical = process.env.appMeta?.canonical
+  const description = process.env.appMeta?.description
   const date = process.env.appMeta?.date
   const year = process.env.appMeta?.year
 
@@ -23,6 +25,102 @@ export default function App({ Component, pageProps }) {
         <meta property="og:image" content={process.env.appMeta?.ogImage} />
         <meta property="og:image:secure_url" content={process.env.appMeta?.ogImage} />
         <meta name="twitter:image" content={process.env.appMeta?.ogImage} />
+
+        <meta property="og:type" content="website" />
+        <meta property="fb:app_id" content="2221664741450677" />
+        <meta property="og:site_name" content={siteName} />
+
+        <meta property="og:title" content={`${brand} - ${title} - ${siteName}`} />
+        <meta name="twitter:title" content={`${brand} - ${title} - ${siteName}`} />
+
+        <meta property="og:description" content={description} />
+        <meta name="twitter:description" content={description} />
+        <meta name="description" content={description} />
+
+        <link rel="canonical" href={canonical} />
+        <meta property="og:url" content={canonical} />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="og:image:type" content="image/jpeg" />
+        <meta property="og:image:width" content="1920" />
+        <meta property="og:image:height" content="1080" />
+
+        {/* Favicons, App Icons and Theme  */}
+        <link
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="https://www.nine.com.au/static/assets/images/apple-touch-icon-57x57-6b5acb5f.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="https://www.nine.com.au/static/assets/images/apple-touch-icon-60x60-dcaa3fbe.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="https://www.nine.com.au/static/assets/images/apple-touch-icon-72x72-ac8692ed.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="https://www.nine.com.au/static/assets/images/apple-touch-icon-76x76-1896d58d.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="https://www.nine.com.au/static/assets/images/apple-touch-icon-114x114-50bb97a0.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="https://www.nine.com.au/static/assets/images/apple-touch-icon-120x120-9e37a545.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="https://www.nine.com.au/static/assets/images/apple-touch-icon-144x144-fe87d88d.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="https://www.nine.com.au/static/assets/images/apple-touch-icon-152x152-ea7e5da8.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="https://www.nine.com.au/static/assets/images/apple-touch-icon-180x180-f11bc70c.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="https://www.nine.com.au/static/assets/images/favicon-32x32-861c8c96.png"
+          sizes="32x32"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="https://www.nine.com.au/static/assets/images/favicon-194x194-c025fffc.png"
+          sizes="194x194"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="https://www.nine.com.au/static/assets/images/favicon-96x96-2a132643.png"
+          sizes="96x96"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="https://www.nine.com.au/static/assets/images/android-chrome-192x192-443fa2ea.png"
+          sizes="192x192"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="https://www.nine.com.au/static/assets/images/favicon-16x16-84d42972.png"
+          sizes="16x16"
+        />
       </Head>
 
       {/* {!isDev &&
