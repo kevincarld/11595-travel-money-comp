0d9c5cfd8c82f69083df365708bc7ea661d1bc5d
Kevin David <kevincarld@gmail.com>
2023-04-17 11:41:58 +1000

    form bg animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
-
 // chakra
 import { Box } from "@chakra-ui/react"
 //
@@ -7,6 +6,7 @@ import Hero from "components/Hero"
 import Form from "components/Form/Form"
 import Img from "components/util/Img"
 import Masthead from "components/Masthead"
+import { motion } from "framer-motion"
 
 export default function Home() {
   return (
@@ -15,8 +15,12 @@ export default function Home() {
 
       <Hero />
 
-      <Box pos='relative' id='intro' h={{d: '1080px'}} >
-        <Img display={{base: 'none', d:'block'}} dimension='1920x1080' src='./form-bg.jpg' alt='form-bg' fit='cover' w='full' h='full' />
+      <Box pos='relative' id='intro' h={{d: '1080px'}} overflow='hidden' >
+        <Img as={motion.img} display={{base: 'none', d:'block'}} dimension='1920x1080' src='./form-bg.jpg' alt='form-bg' fit='cover' w='full' h='full'
+          initial={{scale: 1.15}}
+          whileInView={{scale: 1, transition: {duration: 1}}}
+          viewport={{once: true, margin: '0px 0px -100px 0px'}}
+          />
 
         <Box pos={{d: 'absolute'}}  inset='0 0 0 0' display={{d: 'flex'}} justifyContent='center' alignItems='center'>
           <Form />
